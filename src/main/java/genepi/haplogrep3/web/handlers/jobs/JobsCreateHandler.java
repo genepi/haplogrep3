98e796e80da0feb81f120d98c56476a2b74977e7
Lukas Forer <lukas.forer@i-med.ac.at>
2022-12-16 15:26:10 +0100

    Add baseUrl to templates

--- src/main/java/genepi/haplogrep3/web/handlers/jobs/JobsCreateHandler.java
+++ src/main/java/genepi/haplogrep3/web/handlers/jobs/JobsCreateHandler.java
@@ -82,7 +82,7 @@ public class JobsCreateHandler extends AbstractHandler {
 
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("job", job.getId());
-		String path = RouteUtil.path(JobsShowHandler.PATH, params);
+		String path = RouteUtil.path(configuration.getBaseUrl() + JobsShowHandler.PATH, params);
 
 		context.redirect(path);
 
