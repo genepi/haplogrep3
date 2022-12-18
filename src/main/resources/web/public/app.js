ba6d2bdca87a780faeedccdfd755d1e69bbe75cd
Lukas Forer <lukas.forer@i-med.ac.at>
2022-12-18 12:10:10 +0100

    Destroy datatable to fix bug with fixed table header

--- src/main/resources/web/public/app.js
+++ src/main/resources/web/public/app.js
@@ -4,3 +4,10 @@ function waitDialog() {
     message: '<p>Uploading data</p><div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 100%"><span class="sr-only">45% Complete</span></div></div>'
   });
 }
+
+
+document.addEventListener("turbolinks:visit", function() {
+  if(window.table){
+    window.table.destroy();
+  }
+});
