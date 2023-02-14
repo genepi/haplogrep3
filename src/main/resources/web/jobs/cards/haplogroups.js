c8eb0a11588a7fbff2b531ca82d4b3bc6bb5abdd
Lukas Forer <lukas.forer@i-med.ac.at>
2023-02-14 09:58:13 +0100

    Switch from plotly to chartjs to be compatible with graphvizjs

--- src/main/resources/web/jobs/cards/haplogroups.js
+++ src/main/resources/web/jobs/cards/haplogroups.js
@@ -1,30 +1,47 @@
-var data = [{
-  values: statistics.groups[0].values,
-  labels: statistics.groups[0].clades,
-  type: 'pie'
-}];
+var chart = undefined;
 
-var layout = {
-  //height: 600
-  //width: 700
-  margin: {
-    l: 0,
-    b: 0,
-    t: 0,
-    r: 0
+var Tableau20 = ['#4E79A7', '#A0CBE8', '#F28E2B', '#FFBE7D', '#59A14F', '#8CD17D', '#B6992D', '#F1CE63', '#499894', '#86BCB6', '#E15759', '#FF9D9A', '#79706E', '#BAB0AC', '#D37295', '#FABFD2', '#B07AA1', '#D4A6C8', '#9D7660', '#D7B5A6'];
+
+
+function drawChart(index){
+  var data = {
+    labels: statistics.groups[index].clades,
+    datasets: [{
+      label: 'Samples',
+      data: statistics.groups[index].values,
+      hoverOffset: 4,
+      backgroundColor: Tableau20
+    }]
+  };
+
+  var config = {
+    type: 'pie',
+    data: data,
+    options: {
+      plugins: {
+        legend: {
+          position: 'right'
+        }
+      },
+      animation: {
+        animateRotate: false
+      }
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  if (chart != undefined){
+    chart.destroy();
   }
-};
 
-Plotly.newPlot('plot-haplogroups', data, layout);
+  chart = new Chart(
+      document.getElementById('acquisitions'), config);
+}
 
 $('#update-plot-haplogroups').on('change', function() {
   var index = $(this).val();
-  console.log(index);
-  var data = [{
-    values: statistics.groups[index].values,
-    labels: statistics.groups[index].clades,
-    type: 'pie'
-  }];
-
-  Plotly.newPlot('plot-haplogroups', data, layout);
+  drawChart(index);
 })
+
+drawChart(0);
