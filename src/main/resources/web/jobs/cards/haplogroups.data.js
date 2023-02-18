a843952f5ca1380f6ac55a6af7e41cbd7aed9509
Lukas Forer <lukas.forer@i-med.ac.at>
2023-02-18 12:46:22 +0100

    Create class for haplogroup card

--- src/main/resources/web/jobs/cards/haplogroups.data.js
+++
@@ -1,1 +0,0 @@
-var statistics = {{json(job.statistics)}};
--- src/main/resources/web/jobs/cards/haplogroups.js
+++ src/main/resources/web/jobs/cards/haplogroups.js
@@ -1,63 +1,73 @@
-var Tableau20 = ['#4E79A7', '#A0CBE8', '#F28E2B', '#FFBE7D', '#59A14F', '#8CD17D', '#B6992D', '#F1CE63', '#499894', '#86BCB6', '#E15759', '#FF9D9A', '#79706E', '#BAB0AC', '#D37295', '#FABFD2', '#B07AA1', '#D4A6C8', '#9D7660', '#D7B5A6'];
+class Haplogroups {
 
-function initChart(id, dataset, index){
+  colors = ['#4E79A7', '#A0CBE8', '#F28E2B', '#FFBE7D', '#59A14F', '#8CD17D',
+    '#B6992D', '#F1CE63', '#499894', '#86BCB6', '#E15759', '#FF9D9A', '#79706E',
+    '#BAB0AC', '#D37295', '#FABFD2', '#B07AA1', '#D4A6C8', '#9D7660', '#D7B5A6'
+  ];
 
-  var data = {
-    labels: dataset.groups[index].clades,
-    datasets: [{
-      label: 'Samples',
-      data: dataset.groups[index].values,
-      hoverOffset: 4,
-      backgroundColor: Tableau20
-    }]
-  };
+  chart;
 
-  var config = {
-    type: 'pie',
-    data: data,
-    options: {
-      plugins: {
-        legend: {
-          position: 'right',
-          labels: {
-            boxWidth: 20
+  dataset;
+
+  index = 0;
+
+  constructor(id) {
+    this.id = id;
+  }
+
+  setDataset(dataset) {
+    this.dataset = dataset;
+  }
+
+  setIndex(index) {
+    this.index = index;
+  }
+
+  draw() {
+
+    this.destroy();
+
+    var group = this.dataset.groups[this.index];
+
+    var data = {
+      labels: group.clades,
+      datasets: [{
+        label: 'Samples',
+        data: group.values,
+        hoverOffset: 4,
+        backgroundColor: this.colors
+      }]
+    };
+
+    var config = {
+      type: 'pie',
+      data: data,
+      options: {
+        plugins: {
+          legend: {
+            position: 'right',
+            labels: {
+              boxWidth: 20
+            }
           }
+        },
+        animation: {
+          animateRotate: false
         }
       },
-      animation: {
-        animateRotate: false
-      }
-    },
-    responsive: true,
-    maintainAspectRatio: false
-  };
+      responsive: true,
+      maintainAspectRatio: false
+    };
 
-  return new Chart(document.getElementById(id), config);
+    this.chart = new Chart(document.getElementById(this.id), config);
 
-}
-
-function destroyChart(chart){
-  if (chart != undefined){
-    chart.destroy();
-    window.chart = undefined;
   }
-}
 
-function onChangeClassification(){
-  var index = $(this).val();
-  destroyChart(window.chart);
-  window.chart = initChart('plot-haplogroups', statistics, index);
-}
+  destroy() {
+    if (this.chart != undefined) {
+      this.chart.destroy();
+      this.chart = undefined;
+    }
+  }
 
-//init chart
-destroyChart(window.chart);
-window.chart = initChart('plot-haplogroups', statistics, 0);
-$('#update-plot-haplogroups').on('change', onChangeClassification);
-
-//register visit handler only once.
-if (!window.haplogroupsLoaded){
-  document.addEventListener("turbolinks:visit", function() {
-    destroyChart(window.chart);
-  });
-  window.haplogroupsLoaded = true;
 }
