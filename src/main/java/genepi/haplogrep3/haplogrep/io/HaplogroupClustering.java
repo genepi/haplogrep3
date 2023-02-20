3fc110e2955d88a43d5856fedc84c0ccd2d4a16a
seppinho <sebastian.schoenherr@i-med.ac.at>
2023-02-20 15:53:46 +0100

    Make clustering ordering independent

--- src/main/java/genepi/haplogrep3/haplogrep/io/HaplogroupClustering.java
+++ src/main/java/genepi/haplogrep3/haplogrep/io/HaplogroupClustering.java
@@ -6,34 +6,57 @@ import genepi.io.table.writer.CsvTableWriter;
 
 public class HaplogroupClustering {
 
-	static String[] TOPLEVEL_HAPLOGROUPS_TOP_DOWN = new String[] { "L0", "L1", "L5", "L2", "L6", "L4", "L3", "M", "C", "Z", "E",
-			"G", "Q", "D", "N", "Y", "A", "O", "S", "F", "B", "P", "I", "W", "X", "R", "HV", "V", "H", "J", "T", "U",
-			"K" };
+	static String[] TOPLEVEL_HAPLOGROUPS_TOP_DOWN = new String[] { "L0", "L1", "L5", "L2", "L6", "L4", "L3", "M", "C",
+			"Z", "E", "G", "Q", "D", "N", "Y", "A", "O", "S", "F", "B", "P", "I", "W", "X", "R", "HV", "V", "H", "J",
+			"T", "U", "K"};
 	
-	static String[] TOPLEVEL_HAPLOGROUPS_DOWN_TOP = new String[] { "K", "U", "T", "J", "H", "V", "HV", "R", "X", "W", "I", "P",
-			"B", "F", "S", "O", "A", "Y", "N", "D", "Q", "G", "E", "Z", "C", "M", "L3", "L4", "L6", "L2", "L5", "L1",
-			"L0" };
-
 	public HaplogroupClustering(Phylotree phylotree, String filename) {
 
 		CsvTableWriter writer = new CsvTableWriter(filename, '\t');
 
-		String[] columnsWrite = { "Haplogroup", "Super Haplogroup" };
+		String[] columnsWrite = { "Haplogroup", "Super Haplogroup", "Distance" };
 		writer.setColumns(columnsWrite);
+
+		phylotree.Phylotree phylotreeInstance = phylotree.getPhylotreeInstance();
 
 		for (Haplogroup haplogroup : phylotree.getHaplogroups()) {
 
+			int distanceTmp = -1;
+			String topLevelTmp = null;
+
 			for (String topLevelHaplogroup : TOPLEVEL_HAPLOGROUPS_TOP_DOWN) {
-				boolean hit = new Haplogroup(topLevelHaplogroup).isSuperHaplogroup(phylotree.getPhylotreeInstance(),
-						haplogroup);
+
+				boolean hit = new Haplogroup(topLevelHaplogroup).isSuperHaplogroup(phylotreeInstance, haplogroup);
 
 				if (hit) {
-					writer.setString("Haplogroup", haplogroup.toString());
-					writer.setString("Super Haplogroup", topLevelHaplogroup);
-					writer.next();
-					break;
+
+					try {
+
+						int distance = phylotreeInstance
+								.getDistanceBetweenHaplogroups(new Haplogroup(topLevelHaplogroup), haplogroup);
+						
+						if (distanceTmp == -1 || distance <= distanceTmp) {
+
+							topLevelTmp = topLevelHaplogroup;
+							distanceTmp = distance;
+
+						}
+
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 
+			}
+
+			// TODO Haplogroups like H2a2a1 dont have a super haplogroup. Problem with
+			// parsing?
+			if (topLevelTmp != null) {
+				writer.setString("Haplogroup", haplogroup.toString());
+				writer.setString("Super Haplogroup", topLevelTmp);
+				writer.setInteger("Distance", distanceTmp);
+				writer.next();
 			}
 
 		}
