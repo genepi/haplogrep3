89de71d14c0ae85e32d6ba9961c4b5e808aa0cfb
Lukas Forer <lukas.forer@i-med.ac.at>
2023-02-26 09:40:10 +0100

    Add navbar to configuration to include custom links

---
+++ src/main/java/genepi/haplogrep3/config/NavbarLink.java
@@ -0,0 +1,35 @@
+package genepi.haplogrep3.config;
+
+public class NavbarLink {
+
+	private String text;
+
+	private String url;
+
+	private String icon;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+}
