ab194d472e8837f2fceac62840ca96bbb2f541f7
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2023-02-28 14:27:38 +0100

    Prepare release v3.1.0

--- src/main/java/genepi/haplogrep3/App.java
+++ src/main/java/genepi/haplogrep3/App.java
@@ -25,9 +25,9 @@ public class App implements Runnable {
 
 	public static final String NAME = "Haplogrep 3";
 
-	public static final String VERSION = "3.0.3";
+	public static final String VERSION = "3.1.0";
 
-	public static final String COPYRIGHT = "(c) 2022 Sebastian Schönherr, Hansi Weissensteiner, Lukas Forer";
+	public static final String COPYRIGHT = "(c) 2022-2023 Sebastian Schönherr, Hansi Weissensteiner, Lukas Forer";
 
 	public static final int PORT = 7000;
 
